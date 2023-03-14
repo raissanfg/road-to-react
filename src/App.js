f19d3008d455fb753327e40e4fdf2772ac53c8d9
raissanfg <99144281+raissanfg@users.noreply.github.com>
2023-03-14 12:17:45 -0500

    5-lists-in-React git log git commit -a 5-lists-in-React

--- src/App.js
+++ src/App.js
@@ -1,19 +1,57 @@
 
 import * as React from 'react';
 
-function getTitle(title) {
-  return title;
-}
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  }, 
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+ ];
+
 
 function App() {
   return (
     <div>
-      <h1>Hello {getTitle('World')}</h1>
+      <h1>My Hacker Stories</h1>
 
       <label htmlFor="search">Search:</label>
       <input id="search" type="text"/>
+
+      <hr /> {/* horizontal rule */}
+
+      {/*  render the list  */}
+      
+      <ul>
+        {list.map(function(item) {
+          return (
+            <li key={item.objectID}>
+              <span>
+                 <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.autor}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </li>
+          );
+        })}
+      </ul>
     </div> 
   );
 }
+
+
 
 export default App;
