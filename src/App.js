39b90a64ae25d2c602dbc9cbc937e91bacad051f
raissanfg <99144281+raissanfg@users.noreply.github.com>
2023-03-14 09:44:59 -0500

    Embeded JS in JSX

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 
 import * as React from 'react';
+
+function getTitle(title) {
+  return title;
+}
+
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
-</div> );
+      <h1>Hello {getTitle('World')}</h1>
+
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text"/>
+    </div> 
+  );
 }
 
 export default App;
-
